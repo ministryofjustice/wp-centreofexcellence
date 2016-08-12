60df1e979133af42e24a9fec65d2cea767bf685a
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2016-08-12 16:22:35 +0100

    Improve handling of links.
    - Only show attachment file extension is the file has been set.
    - Always show the extension in lowercase.
    - Improve handling for files without extensions.
    - Only add rel="external" to links if they are for an external resource.

--- templates/link-list-li.php
+++ templates/link-list-li.php
@@ -1,15 +1,34 @@
 <?php if ($link['type'] == 'Attachment'): ?>
   <?php
-  $filenameParts = explode('.', $link['file']['filename']);
-  $ext = array_pop($filenameParts);
+
+  // Get file extension
+  $filename = $link['file']['filename'];
+  $period = strrpos($filename, '.');
+  if ($period !== false) {
+    $ext = strtolower(substr($filename, $period + 1));
+  } else {
+    $ext = false;
+  }
+
   ?>
   <li>
     <a href="<?php echo $link['file']['url']; ?>"><?php echo $link['name']; ?></a>
-    <span class="help-text">(<?php echo $ext; ?>)</span>
+    <?php if ($ext): ?>
+      <span class="help-text">(<?php echo $ext; ?>)</span>
+    <?php endif; ?>
   </li>
 <?php elseif ($link['type'] == 'Link'): ?>
+  <?php
+
+  // Determine if this is an external link
+  $url = $link['link'];
+  $has_protocol = (strpos($url, '://') !== false);
+  $is_external_domain = (stripos($url, get_home_url()) !== 0);
+  $external = ($has_protocol && $is_external_domain);
+
+  ?>
   <li>
-    <a href="<?php echo $link['link']; ?>" rel="external">
+    <a href="<?php echo $link['link']; ?>"<?php if ($external) { ?> rel="external" target="_blank" <?php } ?>>
       <?php echo $link['name']; ?>
     </a>
   </li>
